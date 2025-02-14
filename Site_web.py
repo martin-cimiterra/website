b4849cd41e017cad45683739b1533975c9e5d5d8
martin-cimiterra <martin.cimiterra@gmail.com>
2025-02-14 14:37:28 +0100

    Mise à jour pour déploiement Render

--- Site_web.py
+++ Site_web.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -22,6 +23,6 @@ def teaching():
 def work_in_progress():
     return render_template('work_in_progress.html', title="Work in Progress")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
